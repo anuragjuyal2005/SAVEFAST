1452850e54266d3eae6e192c831ff7cc910e0bc8
anuragjuyal2005 <anuragjuyal2005@gmail.com>
2026-06-22 21:55:53 +0530

    Fix download URL

--- backend/controllers/fileControllers.js
+++ backend/controllers/fileControllers.js
@@ -25,7 +25,9 @@ export const uploadFile = async (req, res) => {
 
     const result = await new Promise((resolve, reject) => {
       const stream = cloudinary.uploader.upload_stream(
-        { resource_type: mimetype === "application/pdf" ? "raw" : "image" },
+        {
+          resource_type: mimetype === "application/pdf" ? "raw" : "image",
+        },
         (error, result) => {
           if (error) reject(error);
           else resolve(result);
